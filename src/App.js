6f77f20fbbdcfc9e068e282a0c9d6ac20dc4d385
akhilpuvvada450 <akhilpuvvada450@gmail.com>
2021-07-18 12:23:39 -0500

    Chaning to  Web

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React By Simi
+          Learn React By Web
         </a>
       </header>
     </div>
