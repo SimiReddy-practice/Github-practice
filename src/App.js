67e544e21083afc52daf90c142c873cc05ac24b8
akhilpuvvada450 <akhilpuvvada450@gmail.com>
2021-07-22 23:26:37 -0500

    feature-qa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React fast Simi!! + master branch
+          Learn React fast Simi!! + Test-Merge-Conflicts-Branch
         </a>
       </header>
     </div>
