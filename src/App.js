6d6d53ec3c4fd041d2245a1b8b6237eb6a1303b0
akhilpuvvada450 <akhilpuvvada450@gmail.com>
2021-07-22 23:29:08 -0500

    line 1

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React fast Simi!! + Test-Merge-Conflicts-Branch
+          Line 1
         </a>
       </header>
     </div>
